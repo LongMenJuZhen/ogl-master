7ae33c912525ddc6b94e694c8dfad3be1e49c1e7
LongMenJuZhen <cyc313@outlook.com>
2024-04-08 15:55:12 +0800

    我已经完全理解tt5咧。

--- tutorial05_textured_cube/tutorial05.cpp
+++ tutorial05_textured_cube/tutorial05.cpp
@@ -64,7 +64,7 @@ int main( void )
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader.glsl", "TextureFragmentShader.fragmentshader.glsl" );
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
@@ -83,8 +83,8 @@ int main( void )
 	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
 	// Load the texture using any two methods
-	GLuint Texture = loadBMP_custom("uvtemplate.bmp");
-	// GLuint Texture = loadDDS("uvtemplate.DDS");
+	// GLuint Texture = loadBMP_custom("uvtemplate.bmp");
+	GLuint Texture = loadDDS("uvtemplate.DDS");
 	
 	// Get a handle for our "myTextureSampler" uniform
 	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
@@ -131,7 +131,7 @@ int main( void )
 	};
 
 	// Two UV coordinatesfor each vertex. They were created with Blender.
-	/*
+	
 	static const GLfloat g_uv_buffer_data[] = { //this UV is for DDS.
 		0.000059f, 0.000004f, 
 		0.000103f, 0.336048f, 
@@ -170,47 +170,47 @@ int main( void )
 		1.000004f, 0.671847f, 
 		0.667979f, 0.335851f
 	};
-	*/
+	
 
 	
-	static const GLfloat g_uv_buffer_data[] = {  //this UV is for BMP.
-		0.000059f, 1.0f-0.000004f, 
-		0.000103f, 1.0f-0.336048f, 
-		0.335973f, 1.0f-0.335903f, 
-		1.000023f, 1.0f-0.000013f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.999958f, 1.0f-0.336064f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.336024f, 1.0f-0.671877f, 
-		0.667969f, 1.0f-0.671889f, 
-		1.000023f, 1.0f-0.000013f, 
-		0.668104f, 1.0f-0.000013f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.000059f, 1.0f-0.000004f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.336098f, 1.0f-0.000071f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.336024f, 1.0f-0.671877f, 
-		1.000004f, 1.0f-0.671847f, 
-		0.999958f, 1.0f-0.336064f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.668104f, 1.0f-0.000013f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.667979f, 1.0f-0.335851f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.668104f, 1.0f-0.000013f, 
-		0.336098f, 1.0f-0.000071f, 
-		0.000103f, 1.0f-0.336048f, 
-		0.000004f, 1.0f-0.671870f, 
-		0.336024f, 1.0f-0.671877f, 
-		0.000103f, 1.0f-0.336048f, 
-		0.336024f, 1.0f-0.671877f, 
-		0.335973f, 1.0f-0.335903f, 
-		0.667969f, 1.0f-0.671889f, 
-		1.000004f, 1.0f-0.671847f, 
-		0.667979f, 1.0f-0.335851f
-	};
+	// static const GLfloat g_uv_buffer_data[] = {  //this UV is for BMP.
+	// 	0.000059f, 1.0f-0.000004f, 
+	// 	0.000103f, 1.0f-0.336048f, 
+	// 	0.335973f, 1.0f-0.335903f, 
+	// 	1.000023f, 1.0f-0.000013f, 
+	// 	0.667979f, 1.0f-0.335851f, 
+	// 	0.999958f, 1.0f-0.336064f, 
+	// 	0.667979f, 1.0f-0.335851f, 
+	// 	0.336024f, 1.0f-0.671877f, 
+	// 	0.667969f, 1.0f-0.671889f, 
+	// 	1.000023f, 1.0f-0.000013f, 
+	// 	0.668104f, 1.0f-0.000013f, 
+	// 	0.667979f, 1.0f-0.335851f, 
+	// 	0.000059f, 1.0f-0.000004f, 
+	// 	0.335973f, 1.0f-0.335903f, 
+	// 	0.336098f, 1.0f-0.000071f, 
+	// 	0.667979f, 1.0f-0.335851f, 
+	// 	0.335973f, 1.0f-0.335903f, 
+	// 	0.336024f, 1.0f-0.671877f, 
+	// 	1.000004f, 1.0f-0.671847f, 
+	// 	0.999958f, 1.0f-0.336064f, 
+	// 	0.667979f, 1.0f-0.335851f, 
+	// 	0.668104f, 1.0f-0.000013f, 
+	// 	0.335973f, 1.0f-0.335903f, 
+	// 	0.667979f, 1.0f-0.335851f, 
+	// 	0.335973f, 1.0f-0.335903f, 
+	// 	0.668104f, 1.0f-0.000013f, 
+	// 	0.336098f, 1.0f-0.000071f, 
+	// 	0.000103f, 1.0f-0.336048f, 
+	// 	0.000004f, 1.0f-0.671870f, 
+	// 	0.336024f, 1.0f-0.671877f, 
+	// 	0.000103f, 1.0f-0.336048f, 
+	// 	0.336024f, 1.0f-0.671877f, 
+	// 	0.335973f, 1.0f-0.335903f, 
+	// 	0.667969f, 1.0f-0.671889f, 
+	// 	1.000004f, 1.0f-0.671847f, 
+	// 	0.667979f, 1.0f-0.335851f
+	// };
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
